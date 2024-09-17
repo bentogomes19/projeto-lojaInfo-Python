d2c17f02edba798c6e5f355de76a5d1f03c077fd
bentogomes19 <bento.gomes@yandex.com>
2024-09-17 18:08:07 -0300

    Módulo com operações SQLite

--- database.py
+++ database.py
@@ -47,6 +47,30 @@ class db():
         self.ConnectDB()
         self.cursor.execute('''UPDATE Clientes SET nome = ? WHERE cod_cliente = ?''',   (novo_nome, cod_cliente))
         self.db.commit()
+        
+    # MÉTODO ALTERAR DADO SOMENTE DO EMAIL
+    def UpdateEmailTabelaClientes(self, novo_email, cod_cliente):
+        self.ConnectDB()
+        self.cursor.execute('''UPDATE Clientes SET email = ? WHERE cod_cliente = ?''', (novo_email, cod_cliente))
+        self.db.commit()
+        
+    # MÉTODO ALTERAR DADO SOMENTE DO TELEFONE
+    def UpdateTelTabelaClientes(self, novo_tel, cod_cliente):
+        self.ConnectDB()
+        self.cursor.execute('''UPDATE Clientes SET telefone = ? WHERE cod_cliente = ?''', (novo_tel, cod_cliente))
+        self.db.commit()
+    
+    # MÉTODO ALTERAR DADO SOMENTE DO CPF
+    def UpdateCPF_TabelaClientes(self, novo_cpf, cod_cliente):
+        self.ConnectDB()
+        self.cursor.execute('''UPDATE Clientes SET cpf = ? WHERE cod_cliente = ?''', (novo_cpf, cod_cliente))
+        self.db.commit()
+
+    # MÉTODO ALTERAR DADO SOMENTE DA SITUAÇÃO
+    def UpdateSituacaoTabelaClientes(self, nova_situacao, cod_cliente):
+        self.ConnectDB()
+        self.cursor.execute('''UPDATE Clientes SET situacao = ? WHERE cod_cliente = ?''', (nova_situacao, cod_cliente))
+        self.db.commit() 
 
     # MÉTODO QueryDataTabelaClientes -> Faz uma query no banco de dados 
     def QueryDataTabelaClientes(self):
@@ -64,14 +88,13 @@ class db():
             return self.linha   
         else:
             return None
-        
-        
     
     # PRODUTOS
     #
     #
     # PRODUTOS 
     
+    # MÉTODO PARA GERAR A TABELA DOS PRODUTOS
     def TabelaProdutos(self):
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS Produtos (
             cod_produto INTEGER PRIMARY KEY,
@@ -82,6 +105,54 @@ class db():
             data_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             )
             ''')
+        
+    # MÉTODO PARA INSERIR OS DADOS NA TABELA PRODUTOS
+    def InsertDataTabelaProdutos(self, cod_produto, marca, nome_produto, quantidade_estoque, valor_unit):
+        self.ConnectDB()
+        self.cursor.execute('''INSERT INTO Produtos (cod_produto, marca, nome_produto, quantidade_estoque, valor_unit) 
+                            VALUES (?,?,?,?,?)''', (cod_produto, marca, nome_produto, quantidade_estoque, valor_unit))
+        self.db.commit()
+        
+    # MÉTODO PARA BUSCAR TODOS OS DADOS CADASTRADOS NA TABELA PRODUTOS
+    def QueryTabelaProdutos(self):
+        self.ConnectDB()
+        self.cursor.execute('''SELECT cod_produto, marca, nome_produto, quantidade_estoque, valor_unit FROM Produtos''')
+        self.linhas = self.cursor.fetchall()
+    
+    # MÉTODO PARA BUSCAR UM PRODUTO ESPECÍFICO NA TABLEA PRODUTOS
+    def BuscarProduto(self, cod_produto):
+        self.ConnectDB()
+        self.cursor.execute("SELECT cod_produto, marca, nome_produto, quantidade_estoque, valor_unit FROM Produtos WHERE cod_produto = ?", (cod_produto,))
+        self.linha = self.cursor.fetchone()
+        if self.linha:
+            return self.linha
+        else:
+            return None
+        
+    
+    # MÉTODO PARA MODIFICAR OS DADOS DA MARCA DO PRODUTO NA TABELA PRODUTOS
+    def UpdateMarcaTabelaProdutos(self, marca, cod_produto):
+        self.ConnectDB()
+        self.cursor.execute('''UPDATE Produtos SET marca = ? WHERE cod_produto = ?''', (marca, cod_produto))
+        self.db.commit()
+        
+    # MÉTODO PARA MODIFICAR OS DADOS DO NOME DO PRODUTO NA TABELA PRODUTOS
+    def UpdateNomeTabelaProdutos(self, nome, cod_produto):
+        self.ConnectDB()
+        self.cursor.execute('''UPDATE Produtos SET nome_produto = ? WHERE cod_produto = ?''', (nome, cod_produto))
+        self.db.commit()
+        
+    # MÉTODO PARA ALTERAR A QUANTIDADE DE ITENS DE UM PRODUTO NA TABELA PRODUTOS
+    def UpdateQuantidadeTabelaProdutos(self, quantidade, cod_produto):
+        self.ConnectDB()
+        self.cursor.execute('''UPDATE Produtos SET quantidade_estoque = ? WHERE cod_produto = ?''', (quantidade, cod_produto))
+        self.db.commit()
+        
+    # MÉTODO PARA ALTERAR O VALOR UNITÁRIO DE UM PRODUTO NA TABELA PRODUTOS
+    def UpdateValorUnitTabelaProdutos(self, valor_unitario, cod_produto):
+        self.ConnectDB()
+        self.cursor.execute('''UPDATE Produtos SET valor_unit = ? WHERE cod_produto = ?''', (valor_unitario, cod_produto))
+        self.db.commit()
         
     def TabelaVendas(self):
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS Vendas (
