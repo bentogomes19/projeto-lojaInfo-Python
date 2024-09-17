3840812908e87c35970975655f8047106eba0ff5
bentogomes19 <bento.gomes@yandex.com>
2024-09-17 18:06:19 -0300

    Método para cadastrar os clientes

--- clientes.py
+++ clientes.py
@@ -16,10 +16,10 @@ class Clientes(db): # CLASSE MÃE (db) -> CLASSE FILHA Clientes
         self.ConnectDB() # Conecta ao banco de dados
         self.TabelaCLientes()
         
-        os.system("clear") # LIMPA A TELA 
+        os.system("cls") # LIMPA A TELA 
         print("BEM-VINDO AO CADASTRO DE CLIENTES DA LOJA")
-        # INSERÇÃO DOS DADOS NOME, EMAIL, TELEFONE, CPF
         
+        # INSERÇÃO DOS DADOS NOME, EMAIL, TELEFONE, CPF
         self.nome = str(input("Informe o nome: "))
         self.email = str(input("Informe o email: "))
         self.telefone = str(input("Digite o telefone: "))
@@ -43,7 +43,7 @@ class Clientes(db): # CLASSE MÃE (db) -> CLASSE FILHA Clientes
         else:
             # CHAMANDO O MÉTODO DA CLASSE db (InsertDataTabelaClientes) PARA A INSERÇÃO DOS DADOS NA TABELA CLIENTES
             self.InsertDataTabelaClientes(self.cod_cliente ,self.nome, self.email, self.telefone, self.cpf, self.situacao)
-            input("Dados cadastrados com sucesso...Pressione enter para continar")
+            input("Dados cadastrados com sucesso...Pressione enter para continuar")
             
         self.DisconnectDB()
     
@@ -52,7 +52,7 @@ class Clientes(db): # CLASSE MÃE (db) -> CLASSE FILHA Clientes
         if not self.linhas: # CASO NÃO TENHA CLIENTES CADASTRADOS
             input("Nenhum cliente cadastrado no sistema")
             return
-        os.system("clear")
+        os.system("cls") # os.system('clear')
         # MOSTAR OS CLIENTES CADASTRADOS FORMATADOS NA TABELA NO TERMINAL
         print("\n\n\t\t\tLISTA DE CLIENTES CADASTRADOS NO SISTEMA DA LOJA")
         print("-" * 110) 
@@ -63,14 +63,33 @@ class Clientes(db): # CLASSE MÃE (db) -> CLASSE FILHA Clientes
             print(f"{cod_cliente:<7} {nome:<28} {email:<25} {telefone:<18} {cpf:<20} {situacao:<10}")
         input()
         
+    # PARA MODIFICAR OS DADOS DO CLIENTE CADASTRADO (CPF, NOME, EMAIL, SITUAÇÃO)
     def EditarDadosCliente(self, opcao_consulta, dados_cliente):
         cod_cliente, nome, email, telefone, cpf, situacao = dados_cliente
         if opcao_consulta == 1:
             self.nome = str(input("Digite o nome: "))
             self.UpdateNomeTabelaClientes(self.nome, cod_cliente)
-            input("Dados alterados com sucesso...")
-    
-        
+        elif opcao_consulta == 2:
+            self.email = str(input("Digite o email: "))
+            self.UpdateEmailTabelaClientes(self.email, cod_cliente)
+        elif opcao_consulta == 3:
+            self.telefone = str(input("Digite o telefone: "))
+            self.UpdateTelTabelaClientes(self.telefone, cod_cliente)
+        elif opcao_consulta == 4:
+            self.cpf = str(input("Digite o cpf: "))
+            self.UpdateCPF_TabelaClientes(self.cpf, cod_cliente)
+        elif opcao_consulta == 5:
+            # situação_op = 1 -> ATIVO ;;; 2 -> INATIVO
+            self.situacao_op = int(input("Situação do Cliente -> [1] - ATIVO || [02] - INATIVO: "))
+            if(self.situacao_op == 1):
+                self.situacao = 'ATIVO'
+            elif(self.situacao_op == 2):
+                self.situacao = 'INATIVO'
+            self.UpdateSituacaoTabelaClientes(self.situacao, cod_cliente)
+            
+        input("Dados alterados com sucesso")
+                
+            
             
 
         
